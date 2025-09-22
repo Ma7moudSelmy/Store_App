81fffa6056c7109d5bdf9b6610eb767076658893
MahmoudSelmy <Ma7moud.m.selmy1@gmail.com>
2025-09-22 14:08:14 +0300

    add

--- lib/services/categories_Services.dart
+++ lib/services/categories_Services.dart
@@ -1,26 +1,18 @@
-import 'dart:convert';
-
-import 'package:http/http.dart' as http;
 import 'package:store_app/Models/Product_Model.dart';
+import 'package:store_app/helper/api.dart';
 
 class CategoriesServices {
   Future<List<ProductModel>> getCategoriesproduct({
     required String categorieNmae,
   }) async {
-    http.Response response = await http.get(
-      Uri.parse("https://fakestoreapi.com/products/category/$categorieNmae "),
+    List<dynamic> data = await Api().get(
+      url: "https://fakestoreapi.com/products/category/$categorieNmae",
     );
-    if (response.statusCode == 200) {
-      List<dynamic> data = jsonDecode(response.body);
-      List<ProductModel> productsList = [];
-      for (var i = 0; i < data.length; i++) {
-        productsList.add(ProductModel.fromJson(data[i]));
-      }
-      return productsList;
-    } else {
-      throw Exception(
-        'Failed to load products for category ${response.statusCode}',
-      );
+
+    List<ProductModel> productsList = [];
+    for (var i = 0; i < data.length; i++) {
+      productsList.add(ProductModel.fromJson(data[i]));
     }
+    return productsList;
   }
 }
